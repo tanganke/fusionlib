c23b2524df3a4279dfef7b7d95843854637eab24
tanganke <tang.anke@foxmail.com>
2024-02-01 19:17:58 +0800

    add mkdocs

---
+++ docs/index.md
@@ -0,0 +1,9 @@
+# fusionlib Documentation
+
+This is fusionlib project.
+
+## Getting Start
+
+```bash
+pip3 install -U fusionlib
+```
--- setup.py
+++ setup.py
@@ -2,6 +2,5 @@ from setuptools import find_packages, setup
 
 setup(
     name="fusionlib",
-    version="0.1",
     packages=find_packages(),
 )
